a593b0a5e670df5aeb1c3996f6639b842e3da52d
Juanpav78 <juanpav.78d@gmail.com>
2023-07-18 17:25:41 -0500

    __ Actualizacion obtener proyectos al entrar

--- src/layouts/IndexLayout.jsx
+++ src/layouts/IndexLayout.jsx
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import { useEffect } from "react"
-import useProyectos from "../hooks/useProyectos"
 const IndexLayout = () => {
-  const {obtenerProyectos} = useProyectos()
-  useEffect(()=>{
-    obtenerProyectos()
-  },[])
+
   return (
     <>
     <Header />
