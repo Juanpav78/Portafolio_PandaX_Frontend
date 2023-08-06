58d2580bce536a1260ee12ed905c0715b3d1546f
Juanpav78 <juanpav.78d@gmail.com>
2023-08-06 16:09:31 -0500

    reload of home bug

--- src/layouts/IndexLayout.jsx
+++ src/layouts/IndexLayout.jsx
@@ -5,6 +5,16 @@ import useProyectos from "../hooks/useProyectos";
 import { useEffect } from "react";
 import Loading from "../components/Loading";
 import {motion} from "framer-motion"
+
+const comprobar = (proyecto)=>{
+  console.log("hola")
+  if(!proyecto){
+    location.reload();
+    return false
+  }
+  return true
+}
+
 const IndexLayout = () => {
   const navigate = useNavigate()
   const {obtenerProyectos, cargando, proyectos } = useProyectos();
@@ -14,9 +24,9 @@ const IndexLayout = () => {
   if(cargando) {
 
     setTimeout(()=>{
-      if(!proyectos){
-      console.log("hola")
-      navigate(0);
+      let isLoading = comprobar(proyectos);
+      if(isLoading){
+        navigate(0);
       }
     },1200)
    
